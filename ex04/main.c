b98fff2df222ff2bf5bd8366355aa4ba6d17b95b
Octave Rossi <ocrossi@z3r4p5.42lyon.fr>
2025-01-06 16:26:06 +0100

    04 some way to plug unplug a usb device from within the vm

--- ex04/main.c
+++ ex04/main.c
@@ -3,17 +3,17 @@
 #include <linux/kernel.h>
 
 static int __init hello_init(void) {
-    printk(KERN_INFO "Hello, World!\n");
+    printk(KERN_INFO "Hello, USB World!\n");
     return 0;
 }
 
 static void __exit hello_exit(void) {
-    printk(KERN_INFO "Cleaning up module\n");
+    printk(KERN_INFO "Cleaning up USB World module\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("A simple Hello World LKM!");
+MODULE_DESCRIPTION("A simple Hello USB World module!");
 MODULE_AUTHOR("Your Name");
